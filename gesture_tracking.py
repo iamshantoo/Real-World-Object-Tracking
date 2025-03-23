3b63c0a3753620b9abf541c55199e702b5fde5d7
Md. Abul Hasan <shantouap40@gmail.com>
2025-03-24 01:42:08 +0600

    Update gesture_tracking.py
    - modified max number of hands

--- gesture_tracking.py
+++ gesture_tracking.py
@@ -7,7 +7,7 @@ class GestureTracking:
         # Initialize MediaPipe Hands
         self.mp_hands = mp.solutions.hands
         self.hands = self.mp_hands.Hands(static_image_mode=False,
-                                         max_num_hands=2,
+                                         max_num_hands=1,
                                          min_detection_confidence=0.5,
                                          min_tracking_confidence=0.5)
         self.mp_draw = mp.solutions.drawing_utils
